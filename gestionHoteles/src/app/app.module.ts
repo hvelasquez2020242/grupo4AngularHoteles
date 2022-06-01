fce26c2332ff765341e3e397aca556f77038b0e8
Kenneth García <hvelasquez-2020242@kinal.edu.gt>
2022-06-01 17:19:15 -0600

    Login

--- gestionHoteles/src/app/app.module.ts
+++ gestionHoteles/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gestionHoteles/src/app/components/login/login.component.ts
+++ gestionHoteles/src/app/components/login/login.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [UsuarioService]
+
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public usuarioModel: Usuario;
+
+  constructor(private _usuarioService: UsuarioService) {
+    this.usuarioModel = new Usuario(
+      "",
+      "",
+      "",
+      "",
+      ""
+    );
+  }
 
   ngOnInit(): void {
+  }
+  getToken(){
+    this._usuarioService.login(this.usuarioModel, "true").subscribe(
+      (response)=>{
+        console.log(response.token);
+        localStorage.setItem("token", response.token)
+      },
+      (error)=>{
+        console.log(<any>error);
+
+      }
+    )
+  }
+
+  getTokenPromesa(): Promise<any> {
+    return new Promise((resolve, reject)=>{
+      this._usuarioService.login(this.usuarioModel, "true").subscribe(
+        (response)=>{
+          localStorage.setItem("token", response.token)
+          resolve(response);
+        },
+        (error)=>{
+          console.log(<any>error);
+
+        }
+      )
+    })
+  }
+
+  login(){
+    this._usuarioService.login(this.usuarioModel).subscribe(
+      (response)=>{
+
+        this.getTokenPromesa().then(respuesta => {
+          console.log(respuesta);
+          localStorage.setItem('identidad', JSON.stringify(response.usuario))
+
+        })
+      },
+      (error)=>{
+        console.log(<any>error);
+      }
+    );
   }
 
 }
